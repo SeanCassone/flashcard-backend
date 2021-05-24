b422ad5f737e4b4b132c1a12595ee8846b244a37
unknown <sean.cassone@gmail.com>
2021-05-24 19:20:07 -0400

    removed test routes and added cards and decks files

--- src/app.js
+++ src/app.js
@@ -1,30 +1,4 @@
 const express = require("express");
-const morgan = require("morgan");
 const app = express();
-
-const sayHello = (req, res, next) => {
-  console.log(req.query);
-  const name = req.query.name;
-  const content = name ? `Hello, ${name}!` : `Hello!`;
-  res.send(content);
-};
-
-const saySomething = (req, res) => {
-  const greeting = req.params.greeting;
-  const name = req.query.name;
-
-  const content = greeting && name ? `${greeting}, ${name}!` : `${greeting}!`;
-  res.send(content);
-};
-
-const sayGoodbye = (req, res) => {
-  res.send("Sorry to see you go!");
-};
-
-app.get("/say/goodbye", sayGoodbye);
-app.get("/say/:greeting", saySomething);
-app.get("/hello", sayHello);
-
-app.use(sayHello);
 
 module.exports = app;
